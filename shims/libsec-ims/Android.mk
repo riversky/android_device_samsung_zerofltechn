0bde2b92c129a98524aac43af543f59d8cf50a66
riverskyfang <fang.hetian@gmail.com>
2017-12-16 14:04:50 +0800

    add shim library for libsec-ims

--- shims/libsec-ims/Android.mk
+++ shims/libsec-ims/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-PRODUCT_PACKAGES += \
-    atfwd \
-    libsec-ims_shim
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.radio.multisim.config=dsds \
-    persist.cne.feature=0 \
-    persist.radio.lte_vrte_ltd=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.add_power_save=1 \
-    persist.data.netmgrd.qos.enable=false \
-    persist.radio.new.profid=true
+LOCAL_SRC_FILES := \
+    libsec-ims_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := libmedia liblog
+
+LOCAL_MODULE := libsec-ims_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
